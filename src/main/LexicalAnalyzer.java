13da44e65136713fc308f25af515d42bd01f3ffc
Ziang Guo <prisimtower@gmail.com>
2017-12-09 17:42:56 -0600

    added LA

---
+++ src/main/LexicalAnalyzer.java
@@ -0,0 +1,16 @@
+package main;
+
+public class LexicalAnalyzer {
+
+    public static final String EQUAL = "=";
+    public static final String PLUS = "+";
+    public static final String SUBTRACT = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+    public static final String LBRACKET = "(";
+    public static final String RBRACKET = ")";
+    public static final String FACTROIAL = "!";
+    public static final String POWER = "^";
+    public static final String NUMBER = "[0123456789]+(\\.[0123456789]+)*";
+    public static final String VARIABLE = "[abcdefghijklmnopqrstuvwxyz]";
+}
